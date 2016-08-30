7e46ac24f36323234a30b3b6120a12e9dda36844
Юрий Бикузин <yury.bikuzin@gmail.com>
2016-08-30 17:17:36 +0300

    Сделал задел ExerciseResult Добавил иконки приложения Включил iCloudKit

---
+++ Trainer/ExcerciseResult.swift
@@ -0,0 +1,43 @@
+//
+//  ExcerciseResult.swift
+//  Trainer
+//
+//  Created by Юрий Бикузин on 30.08.16.
+//  Copyright © 2016 Yury Bikuzin. All rights reserved.
+//
+
+import Foundation
+
+class ExcerciseResult {
+    let labelText: String
+    let answerText: String
+    let isValidAnswer: Bool
+    let timing: TimeInterval
+    init (labelText: String, answerText: String, isValidAnswer: Bool, timing: TimeInterval) {
+        self.labelText = labelText
+        self.answerText = answerText
+        self.isValidAnswer = isValidAnswer
+        self.timing = timing
+    }
+}
+
+class ExcerciseResultPack {
+    let author: String
+    let title: String
+    var excerciseResults = [ExcerciseResult]()
+    var errorCount: Int {
+        var errorCount = 0
+        for excerciseResult in excerciseResults {
+            if !excerciseResult.isValidAnswer {
+                errorCount += 1
+            }
+        }
+        return errorCount
+    }
+    var isComplete = false
+    var timing: TimeInterval = 0
+    init (author: String, title: String) {
+        self.author = author
+        self.title = title
+    }
+}
